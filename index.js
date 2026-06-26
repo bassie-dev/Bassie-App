e041c78cc755487f65485c8790815db1b4f2289a
Basetsana Mokwena <mokwenabasetsana022@gmail.com>
2026-06-26 17:30:30 +0200

    added index.js file

---
+++ index.js
@@ -0,0 +1,11 @@
+function SearchSubmit(event){
+    event.preventDefault();
+    let searchInput = document.querySelector("#search");
+    let h1 = document.querySelector("h1");
+    h1.innerHTML = searchInput.value;
+}
+
+
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", SearchSubmit);
