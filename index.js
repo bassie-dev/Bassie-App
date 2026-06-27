41a8ccfef0376da341fa252394356566c392d2c5
Basetsana Mokwena <mokwenabasetsana022@gmail.com>
2026-06-27 11:56:43 +0200

    updated html & added API

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 function SearchSubmit(event){
     event.preventDefault();
     let searchInput = document.querySelector("#search");
-    let h1 = document.querySelector("h1");
-    h1.innerHTML = searchInput.value;
+
+    searchCity(searchInput.value);
 }
 
 
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", SearchSubmit);
+
+function searchCity(city){
+    let apiKey = "ff8cb50adfdf253bb46cfe07e0cf92e4";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(refreshWeather);
+}
+
+function refreshWeather(response){
+    let temperatureElement=document.querySelector("#temperature");
+    let cityElement = document.querySelector("h1");
+
+    let temperature = response.data.main.temp;
+    let cityName = response.data.name;
+
+
+cityElement.innerHTML= cityName;
+temperatureElement.innerHTML = Math.round(temperature);
+    }
+    searchCity("Hammanskraal")
